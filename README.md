1b8d1edff2e500bc5870504596d91988e9d094d8
SaneelDaniel <saneel.daniel07@gmail.com>
2021-02-15 19:27:37 -0800

    Late Push

---
+++ README.md
@@ -0,0 +1,18 @@
+## `Decentralized File Storage.`
+
+### Welcome to the New World of File Storage & Sharing
+
+    -- This app uses IPFS to store your files in a de-centralized secure file storing network
+
+    -- The files are mapped with the Ethereum Wallet Account that approved the transaction
+
+    -- This ensures no other account can view/tamper with the documents on another account
+
+    -- The contracts are written in Solidity Language, and tested and deployed using Ganache and Truffle Framework
+
+    -- The tests are written using Chai Framework
+
+    -- The Client Side app is written using React.JS
+
+
+## Screen shots Of the Running App
---
+++ src/components/HelperText.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function HelperText() {
+  return (
+    <div>
+      <br></br>
+      <h3 style={{ justifySelf: "center" }}>
+        Welcome to the New World of File Storage & Sharing
+      </h3>
+      <h5>
+        &emsp;-- This app uses IPFS to store your files in a de-centralized
+        secure file storing network{" "}
+      </h5>
+      <h5>
+        &emsp;-- The files are mapped with the Ethereum Wallet Account that
+        approved the transaction{" "}
+      </h5>
+      <h5>
+        &emsp;-- This ensures no other account can view/tamper with the document{" "}
+      </h5>
+      <h6></h6>
+    </div>
+  );
+}
+
+export default HelperText;
